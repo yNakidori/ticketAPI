f65ac4feaf028ed1f0e14d8d64ddc895b3e20110
Naki <leandroreisubi@gmail.com>
2024-10-31 09:58:10 -0300

    chat component

--- src/components/chatArea_Component/chat/Chat.jsx
+++ src/components/chatArea_Component/chat/Chat.jsx
@@ -1,7 +1,46 @@
 import "./Chat.scss";
+import Avatar from "../../../assets/chat/avatar.png";
+import Phone from "../../../assets/chat/phone.png";
+import Video from "../../../assets/chat/video.png";
+import Info from "../../../assets/chat/info.png";
+import Emoji from "../../../assets/chat/emoji.png";
+import Img from "../../../assets/chat/img.png";
+import Camera from "../../../assets/chat/camera.png";
+import Mic from "../../../assets/chat/mic.png";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src={Avatar} alt="" />
+          <span>Username</span>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div className="icons">
+          <img src={Phone} alt="" />
+          <img src={Video} alt="" />
+          <img src={Info} alt="" />
+        </div>
+      </div>
+
+      <div className="center"></div>
+
+      <div className="bottom">
+        <div className="icons">
+          <img src={Img} alt="" />
+          <img src={Camera} alt="" />
+          <img src={Mic} alt="" />
+        </div>
+        <input type="text" placeholder="Type a message" />
+        <div className="emoji">
+          <img src={Emoji} alt="" />
+        </div>
+        <div className="sendButton">Send</div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
