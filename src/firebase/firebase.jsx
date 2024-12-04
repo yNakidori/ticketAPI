52a061577f6a116a773150331db1acedc1c46f3b
Naki <leandroreisubi@gmail.com>
2024-12-04 17:45:44 -0300

    sidebar

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -21,8 +21,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage();
+const storage = getStorage(app);
 
 //Initialize Firebase Authentication
 export const auth = getAuth(app);
-export { db };
+export { db, storage };
