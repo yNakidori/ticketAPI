5748e17057eb72f6ae1f93e2746468950131348c
Naki <leandroreisubi@gmail.com>
2024-12-08 21:32:41 -0300

    Home page fixes

--- src/components/homePage_Components/personalTickets.jsx
+++ src/components/homePage_Components/personalTickets.jsx
@@ -1,24 +1,21 @@
-
-import React from 'react';
+import React from "react";
 
 const TicketsPessoais = () => {
+  const tickets = [
+    { id: 1, title: "Meu Ticket 1", description: "Descrição do meu ticket 1" },
+    { id: 2, title: "Meu Ticket 2", description: "Descrição do meu ticket 2" },
+  ];
 
-    const tickets = [
-        { id: 1, title: 'Meu Ticket 1', description: 'Descrição do meu ticket 1' },
-        { id: 2, title: 'Meu Ticket 2', description: 'Descrição do meu ticket 2' },
-
-    ];
-
-    return (
-        <div>
-            {tickets.map(ticket => (
-                <div key={ticket.id} className="ticket">
-                    <h3>{ticket.title}</h3>
-                    <p>{ticket.description}</p>
-                </div>
-            ))}
+  return (
+    <div>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="ticket">
+          <h3>{ticket.title}</h3>
+          <p>{ticket.description}</p>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default TicketsPessoais;
